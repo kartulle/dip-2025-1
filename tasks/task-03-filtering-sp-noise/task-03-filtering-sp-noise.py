d94e4904f0beee00aa508a8f51b995a70d02a81c
Karolaine <kartulle@hotmail.com>
2025-08-10 21:01:27 -0300

    task completa

--- tasks/task-03-filtering-sp-noise/task-03-filtering-sp-noise.py
+++ tasks/task-03-filtering-sp-noise/task-03-filtering-sp-noise.py
@@ -1,4 +1,4 @@
-import cv
+import cv2
 import numpy as np
 
 def remove_salt_and_pepper_noise(image: np.ndarray) -> np.ndarray:
@@ -11,10 +11,11 @@ def remove_salt_and_pepper_noise(image: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: Denoised image.
     """
-    # TODO: Implement noise removal here (e.g., median filtering)
-    return image  # Replace this with your filtering implementation
-
+    noisyRemoval = cv2.medianBlur(image, ksize=5)
+    return noisyRemoval  
+    
 if __name__ == "__main__":
-    noisy_image = cv.imread("noisy_image.png", cv.IMREAD_GRAYSCALE)
+    noisy_image = cv2.imread("img/head.png", cv2.IMREAD_GRAYSCALE)
     denoised_image = remove_salt_and_pepper_noise(noisy_image)
-    cv.imwrite("denoised_image.png", denoised_image)
+    cv2.imwrite("img/head_filtered.png", denoised_image)
+
